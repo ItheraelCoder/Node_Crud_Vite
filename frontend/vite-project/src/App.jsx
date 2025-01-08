bae8a7431512e60ced91c69fda66029abee56125
Jorge Edwin Peña <jedwin513@gmail.com>
2025-01-08 08:15:54 -0500

    Agregamos las funciones para el CRUD en el front

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,1 +1,73 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; //*Hooks de react
+import axios from "axios"; //*Axios para hacer peticiones HTTP
+
+const BASE_URL = "http://localhost:3000/usuarios/";
+
+const App = () => {
+  const [users, setUsers] = useState([]); //*Estado para almacenar los usuarios
+  const [newName, setNewName] = useState(""); //*Estado para almacenar el nombre del usuario
+  const [newEmail, setNewEmail] = useState(""); //*Estado para almacenar el email del usuario
+
+  //*Función para obtener los usuarios
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+      .then((res) => {
+        setUsers(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log('Error al obtener los usuarios', err);
+      })
+  }, []);
+
+  //*Función para agregar un usuario
+  const handleCreate = () => {
+    if (nombre.trim() && email.trim()) {
+      axios.post(`${BASE_URL}add`, {
+        nombre: newName,
+        email: newEmail
+      }).then((res) => {
+        setNewName("");
+        setUsers((prevUsers) => [...prevUsers, res.data]);
+        setNewEmail("");
+      }).catch((err) => {
+        console.log('Error al agregar usuario', err);
+      });
+    }
+  };
+
+  //*Función para eliminar un usuario
+  const handleDelete = (id) => {
+    axios.delete(`${BASE_URL}delete/${id}`)
+      .then(() => {
+        setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
+      })
+      .catch((err) => {
+        console.log('Error al eliminar usuario', err);
+      });
+  };
+
+  //*Función para actualizar un usuario
+  const handleUpdate = (id, nombre, email) => {
+    const newName = prompt('Ingrese el nuevo nombre', nombre);
+    const newEmail = prompt('Ingrese el nuevo email', email);
+
+    if (newName !== nombre && newEmail !== email) {
+      axios.put(`${BASE_URL}update/${id}`, {
+        nombre: newName,
+        email: newEmail
+      }).then(() => {
+        setUsers((prevUsers) => {
+          prevUsers.map((user) => {
+            user.id === id ? { ...user, nombre: newName, email: newEmail } : user
+          })
+        });
+      }).catch((err) => {
+        console.log('Error al actualizar usuario', err);
+      });
+    }
+  };
+
+
+
+};
